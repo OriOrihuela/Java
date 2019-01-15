26c5b51a2a8b804ee729af3bb6cf36d96e3feb5a
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-15 14:29:45 +0100

    Making examples from Chapter 4 of Java Beginning Programming

---
+++ JavaExercises/CourseAdministration/src/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    int id;
+    String firstName;
+    String lastName;
+    int birthYear, birthMonth, birthDay;
+
+    boolean isBirthday() {
+        // Return True if it's the student birthday.
+        return false;
+    }
+
+    void giveWarning (boolean isFinalWarning) {
+        // You should study harder!
+    }
+
+    int numberOfFriends() {
+        // Return the number of friends the student has.
+        return 0;
+    }
+}
