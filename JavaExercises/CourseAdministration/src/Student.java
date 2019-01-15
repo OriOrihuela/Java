4093b147966683dc363eb60929b9f2e663062817
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-15 18:29:05 +0100

    Added examples from Chapter 4 of Beginning Java Programming

--- JavaExercises/CourseAdministration/src/Student.java
+++ JavaExercises/CourseAdministration/src/Student.java
@@ -17,4 +17,17 @@ public class Student {
         // Return the number of friends the student has.
         return 0;
     }
+
+    public static void main(String[] args) {
+        Student firstStudent = new Student();
+        Student secondStudent = new Student();
+        firstStudent.id = 1;
+        firstStudent.firstName = "Marc";
+        secondStudent.id = 2;
+        secondStudent.firstName = "Sophie";
+
+        System.out.println("The student object referred to "+
+                "by the variable secondStudent has the first "+
+                "name: "+secondStudent.firstName);
+    }
 }
