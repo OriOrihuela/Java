83962b894cd4ea47176abe7db34f2ad4dc5e15f1
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-16 13:15:30 +0100

    Initial classes done

---
+++ JavaExercises/ArraysMatrixsLists/kataDatDni/src/Dni.java
@@ -0,0 +1,13 @@
+
+public class Dni {
+        private String dni = null;
+        private TablaAsignacion tabla = new TablaAsignacion();
+
+        /* Constructors */
+
+        public Dni(){}
+
+        public Dni(String dni) {
+            this.dni = dni;
+        }
+}
---
+++ JavaExercises/ArraysMatrixsLists/kataDatDni/src/TablaAsignacion.java
@@ -0,0 +1,5 @@
+
+public class TablaAsignacion {
+    char[] tabla = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J',
+            'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' };
+}
