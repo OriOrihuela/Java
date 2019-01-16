9ac73d94f81011a7f2cee147016e996807af0f76
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-16 20:43:54 +0100

    Deleted

--- JavaExercises/ArraysMatrixsLists/kataDatDni/src/TablaAsignacion.java
+++
@@ -1,5 +0,0 @@
-
-public class TablaAsignacion {
-    char[] tabla = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J',
-            'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' };
-}
