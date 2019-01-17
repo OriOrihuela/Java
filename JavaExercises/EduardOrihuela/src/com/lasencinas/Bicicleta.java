d415a39dcf0df590808b5ba94dc13603d2cf31cd
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-17 23:03:48 +0100

    Properties, constructor and setters/getters added

--- JavaExercises/EduardOrihuela/src/com/lasencinas/Bicicleta.java
+++ JavaExercises/EduardOrihuela/src/com/lasencinas/Bicicleta.java
@@ -1,4 +1,16 @@
 package com.lasencinas;
 
 public class Bicicleta {
+
+    // Properties of the Class
+    private int id;
+
+    // Constructor
+    public Bicicleta() {}
+
+    public Bicicleta(int id) { this.id = id; }
+
+    // Setters and Getters
+    public void setId(int number) { this.id = number;}
+    public int getId() { return this.id; }
 }
