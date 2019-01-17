e76215130f3b5e61592256adebacf1a4d056e781
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-17 23:18:31 +0100

    Comments added to properties and setters/getters/

--- JavaExercises/EduardOrihuela/src/com/lasencinas/Bicicleta.java
+++ JavaExercises/EduardOrihuela/src/com/lasencinas/Bicicleta.java
@@ -3,7 +3,7 @@ package com.lasencinas;
 public class Bicicleta {
 
     // Properties of the Class
-    private int id;
+    private int id; /* Identifier of the bike; number of three digits */
 
     // Constructor
     public Bicicleta() {}
@@ -11,6 +11,7 @@ public class Bicicleta {
     public Bicicleta(int id) { this.id = id; }
 
     // Setters and Getters
+            /* ID property --> Setters/Getters */
     public void setId(int number) { this.id = number;}
     public int getId() { return this.id; }
 }
