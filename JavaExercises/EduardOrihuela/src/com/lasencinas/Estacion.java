80a07b7962f198466897ef8373ea2529cb7389c0
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-18 00:00:54 +0100

    Properties, constructor and getters of the class made

--- JavaExercises/EduardOrihuela/src/com/lasencinas/Estacion.java
+++ JavaExercises/EduardOrihuela/src/com/lasencinas/Estacion.java
@@ -1,4 +1,25 @@
 package com.lasencinas;
 
 public class Estacion {
+
+    // Properties of the class
+    private int id;                 /* Identifier of the station */
+    private String direction;       /* Direction of the station */
+    private int numberAnchorages;   /* Number of anchorages in station */
+    private Bicicleta[] anchorages; /* Array of the different bikes stored in the station */
+
+    // Constructor
+    public Estacion() {}
+
+    public Estacion(int id, String direction, int anchorages) {
+        this.id = id;
+        this.direction = direction;
+        this.numberAnchorages = anchorages;
+        this.anchorages = new Bicicleta[anchorages];
+    }
+
+    // Setters adn getters of all properties
+    public int getId() { return this.id; }
+    public String getDirection() { return this.direction;}
+    public  int getNumberAnchorages() { return this.numberAnchorages;}
 }
