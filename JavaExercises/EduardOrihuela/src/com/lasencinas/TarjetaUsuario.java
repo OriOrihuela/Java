a4327304cec9077feef60195b9bd513196a2790c
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-17 23:19:02 +0100

    Class properties, constructor and setters/getters added

--- JavaExercises/EduardOrihuela/src/com/lasencinas/TarjetaUsuario.java
+++ JavaExercises/EduardOrihuela/src/com/lasencinas/TarjetaUsuario.java
@@ -1,4 +1,24 @@
 package com.lasencinas;
 
 public class TarjetaUsuario {
+
+    // Properties
+    private int id; /* Identifier of the card; number of nine digits */
+    private boolean activated;  /* Logical value that indicates if the card is activated or not */
+
+    // Constructor
+    public TarjetaUsuario() {}
+
+    public TarjetaUsuario(int id) { this.id = id;}
+
+    // Setters and Getters of all properties
+            /* ID property --> Setters/Getters */
+    public void setId(int number) { this.id = number;}
+    public int getId() { return this.id;}
+
+            /* Activated property --> Setters/Getters */
+    public void setActivated(boolean sentence) { this.activated = sentence;}
+    public boolean getActivated() { return this.activated;}
+
+
 }
