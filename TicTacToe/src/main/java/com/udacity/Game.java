a099acf060cc021179e494227ad9b2c6eabceaa0
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-23 00:47:55 +0100

    TicTacToe finished

--- TicTacToe/src/main/java/com/udacity/Game.java
+++ TicTacToe/src/main/java/com/udacity/Game.java
@@ -150,7 +150,41 @@ public class Game {
      */
     public String checkGameWinner(char [][]grid){
         String result = "None";
-        //Student code goes here ...
+        boolean win = false;
+
+        // Checks x horizontals and verticals for wins
+        for(int i = 0; i < 3; i++){
+            if(grid[i][0] == 'x' && grid[i][1] == 'x' && grid[i][2] == 'x' || grid[0][i] == 'x' && grid[1][i] == 'x' && grid[2][i] == 'x'){
+                result = "X wins";
+                win = true;
+            }
+        }
+
+        // Checks x diagonals for wins
+        if(grid[0][0] == 'x' && grid[1][1] == 'x' && grid[2][2] == 'x' || grid[2][0] == 'x' && grid[1][1] == 'x' && grid[0][2] == 'x'){
+            result = "X wins";
+            win = true;
+        }
+
+        // Checks o horizontals and verticals for wins
+        for(int i = 0; i < 3; i++){
+            if(grid[i][0] == 'o' && grid[i][1] == 'o' && grid[i][2] == 'o' || grid[0][i] == 'o' && grid[1][i] == 'o' && grid[2][i] == 'o'){
+                result = "O wins";
+                win = true;
+            }
+        }
+
+        // Checks o diagonals for wins
+        if(grid[0][0] == 'o' && grid[1][1] == 'o' && grid[2][2] == 'o' || grid[2][0] == 'o' && grid[1][1] == 'o' && grid[0][2] == 'o'){
+            result = "O wins";
+            win = true;
+        }
+
+        // Checks there are no empty spots and no wins
+        if(freeSpots == 0 && !win){
+            result = "Tie";
+        }
+
         return result;
     }
 
