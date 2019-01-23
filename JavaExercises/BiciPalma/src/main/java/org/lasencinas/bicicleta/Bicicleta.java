6a829086e97204b162c91b4c33302810fdc997f7
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-23 22:04:25 +0100

    New BiciPalma project with Maven

---
+++ JavaExercises/BiciPalma/src/main/java/org/lasencinas/bicicleta/Bicicleta.java
@@ -0,0 +1,18 @@
+package org.lasencinas.bicicleta;
+
+public class Bicicleta {
+
+    // Properties of the Class
+    private int id; /* Identifier of the bike; number of three digits */
+
+    // Constructor
+    public Bicicleta() {}
+
+    public Bicicleta(int id) { this.id = id; }
+
+    // Setters and Getters of all properties
+
+    /* id property */
+    public void setId(int number) { this.id = number; }
+    public int getId() { return this.id; }
+}
---
+++ JavaExercises/BiciPalma/src/main/java/org/lasencinas/tarjetaUsuario/TarjetaUsuario.java
@@ -0,0 +1,26 @@
+package org.lasencinas.tarjetaUsuario;
+
+public class TarjetaUsuario {
+
+    // Properties
+    private int id;             /* Identifier of the card; number of nine digits */
+    private boolean activated;  /* Logical value that indicates if the card is activated or not */
+
+    // Constructor
+    public TarjetaUsuario() {}
+
+    public TarjetaUsuario(int id, boolean activated) {
+        this.id = id;
+        this.activated = activated;
+    }
+
+    // Setters and Getters of all properties
+
+    /* id property */
+    public void setId(int number) { this.id = number; }
+    public int getId() { return this.id; }
+
+    /* activated property */
+    public void setActivated(boolean sentence) { this.activated = sentence; }
+    public boolean getActivated() { return this.activated; }
+}
