c3d8b39e899ba434c4e9eb0ea7219adcd57198d6
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-29 16:06:43 +0100

    Properties and main behaviours implemented

--- JavaExercises/kataBowlingGame/src/main/java/org/lasencinas/ScoreCard.java
+++ JavaExercises/kataBowlingGame/src/main/java/org/lasencinas/ScoreCard.java
@@ -2,4 +2,53 @@ package org.lasencinas;
 
 public class ScoreCard {
 
+    // Properties of the class
+    private int rolls = 0;
+    private String scoreCard = "";
+    private int totalScore = 0;
+
+    //Constructor
+    public ScoreCard() {
+        this.scoreCard = "";
+        this.rolls = 0;
+    }
+
+    public ScoreCard(String scoreCard) {
+        this.scoreCard = scoreCard;
+        this.rolls = scoreCard.length();
+    }
+
+    //Setters and getters of the class
+    public String getScoreCard() {
+        return this.scoreCard;
+    }
+
+    public int getRolls() {
+        return this.rolls;
+    }
+
+    public int getTotalScore() {
+        return this.totalScore;
+    }
+
+    private void setTotalScore(int score) {
+        this.totalScore = score;
+    }
+
+    public int calculateScore(){
+
+        for(int i=0; i < getScoreCard().length(); i++){
+            char pin = scoreCard.charAt(i);
+            if (pin == 'X') {
+                setTotalScore(10);
+            } if (pin == '/') {
+                setTotalScore(10);
+            } if (pin == '-') {
+                continue;
+            } else {
+                // setTotalScore(scoreCard[]);
+            }
+        }
+        return this.totalScore;
+    }
 }
