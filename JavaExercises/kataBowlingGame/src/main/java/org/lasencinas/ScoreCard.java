f142ff8eb0902275fa752f040c80031c5b170026
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-01-29 22:29:19 +0100

    Main class almost done

--- JavaExercises/kataBowlingGame/src/main/java/org/lasencinas/ScoreCard.java
+++ JavaExercises/kataBowlingGame/src/main/java/org/lasencinas/ScoreCard.java
@@ -2,12 +2,15 @@ package org.lasencinas;
 
 public class ScoreCard {
 
-    // Properties of the class
+    // Properties of the class //
+    private int STRIKE = 10;
+    private int ZERO = 0;
     private int rolls = 0;
     private String scoreCard = "";
     private int totalScore = 0;
+    private String pins = "-123456789";
 
-    //Constructor
+    // Constructor //
     public ScoreCard() {
         this.scoreCard = "";
         this.rolls = 0;
@@ -18,7 +21,7 @@ public class ScoreCard {
         this.rolls = scoreCard.length();
     }
 
-    //Setters and getters of the class
+    // Setters and getters of the class //
     public String getScoreCard() {
         return this.scoreCard;
     }
@@ -31,22 +34,42 @@ public class ScoreCard {
         return this.totalScore;
     }
 
-    private void setTotalScore(int score) {
-        this.totalScore = score;
+    // Behaviours //
+    private void updateTotalScore(int score) {
+        this.totalScore += score;
+    }
+
+    public int computePins(String pins) {
+        return this.pins.indexOf(pins);
+    }
+
+    public int getSTRIKE() {
+        return this.STRIKE;
+    }
+
+    public int computeStrike(char strike) {
+        if (strike == 'X') {
+            return getSTRIKE();
+        }
+        else {
+            return 0;
+        }
     }
 
     public int calculateScore(){
 
-        for(int i=0; i < getScoreCard().length(); i++){
-            char pin = scoreCard.charAt(i);
+        for(int i = 0; i < getScoreCard().length(); i++){
+
+            char pin = getScoreCard().charAt(i);
+
             if (pin == 'X') {
-                setTotalScore(10);
-            } if (pin == '/') {
-                setTotalScore(10);
-            } if (pin == '-') {
-                continue;
-            } else {
-                // setTotalScore(scoreCard[]);
+                updateTotalScore(computeStrike(pin));
+            }
+            if (pin == '/') {
+                updateTotalScore(10);
+            }
+            else {
+                updateTotalScore(computePins(i));
             }
         }
         return this.totalScore;
