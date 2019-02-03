3f487842881bdce4e66dbe2b752e1025f47c057d
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-02-03 02:26:40 +0100

    All test cases of Bowling Kata (passed)

---
+++ JavaExercises/kataBowlingGame/src/test/java/org/lasencinas/ScoreCardTest.java
@@ -0,0 +1,73 @@
+package org.lasencinas;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ScoreCardTest {
+
+    private ScoreCard scoreCard = null;
+
+
+    /* -------------- TESTING BEHAVIOURS -------------------*/
+
+    @Before
+    public void init() {
+        scoreCard = new ScoreCard();
+    }
+
+    @Test
+    public void getScoreCardTest() {
+        ScoreCard scoreCard = new ScoreCard("XXXXXXXXXXXX");
+        assertEquals("XXXXXXXXXXXX", scoreCard.getScoreCard());
+    }
+
+    @Test
+    public void computeStrikeTest() {
+        assertEquals(10, scoreCard.computeStrike('X'));
+        assertEquals(0, scoreCard.computeStrike('9'));
+    }
+
+    @Test
+    public void computeSpareTest() {
+        assertEquals(10, scoreCard.computeSpare('/'));
+        assertEquals(10, scoreCard.computeSpare('/'));
+    }
+
+    @Test
+    public void computePinsTest() {
+        assertEquals(0, scoreCard.computePins('-'));
+        assertEquals(5, scoreCard.computePins('5'));
+        assertEquals(9, scoreCard.computePins('9'));
+        /*for (Integer i = 1; i <= 9; i++) {
+            assertEquals(i.intValue(), scoreCard.computePins(i));*/
+    }
+
+    @Test
+    public void calculateScoreTest() {
+        String punctuation = "XXXXXXXXXXXX";
+        int totalScore = scoreCard.calculateScore(punctuation);
+        assertEquals(300, totalScore);
+    }
+
+    @Test
+    public void calculateScoreTest2() {
+        String punctuation = "12345123451234512345";
+        int totalScore = scoreCard.calculateScore(punctuation);
+        assertEquals(60, totalScore);
+    }
+
+    @Test
+    public void calculateScoreTest3() {
+        String punctuation = "5/5/5/5/5/5/5/5/5/5/5";
+        int totalScore = scoreCard.calculateScore(punctuation);
+        assertEquals(150, totalScore);
+    }
+
+    @Test
+    public void calculateScoreTest4() {
+        String punctuation = "9-9-9-9-9-9-9-9-9-9-";
+        int totalScore = scoreCard.calculateScore(punctuation);
+        assertEquals(90, totalScore);
+    }
+}
