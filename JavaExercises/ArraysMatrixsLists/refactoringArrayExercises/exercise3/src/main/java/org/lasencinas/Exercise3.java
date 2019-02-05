454670bd7ccb789852c20127b7651406d223fee8
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-02-05 14:11:45 +0100

    Exercise3 added to remote

---
+++ JavaExercises/ArraysMatrixsLists/refactoringArrayExercises/exercise3/src/main/java/org/lasencinas/Exercise3.java
@@ -0,0 +1,19 @@
+package org.lasencinas;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        int[] vector = new int[30];
+        Random aleatorio = new Random();
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = aleatorio.nextInt(20) + 1;
+        }
+        Arrays.sort(vector);
+        for (int numero : vector) {
+            System.out.println(numero);
+        }
+    }
+}
+
---
+++ JavaExercises/ArraysMatrixsLists/refactoringArrayExercises/exercise3/src/test/java/org/lasencinas/Exercise3Test.java
@@ -0,0 +1,10 @@
+package org.lasencinas;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class Exercise3Test {
+
+}
+
