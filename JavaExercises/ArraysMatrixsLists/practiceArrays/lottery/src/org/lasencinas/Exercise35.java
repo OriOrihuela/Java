5a8332cdf6f69268b4b94be0023407842374cfe7
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-02-11 16:53:44 +0100

    Documentation added

--- JavaExercises/ArraysMatrixsLists/practiceArrays/lottery/src/org/lasencinas/Exercise35.java
+++ JavaExercises/ArraysMatrixsLists/practiceArrays/lottery/src/org/lasencinas/Exercise35.java
@@ -1,3 +1,18 @@
+/**
+ * The Primitive Lottery is a game of chance regulated by the National Organization of State Lotteries and Bets (LAE)
+ * which consists of choosing 6 different numbers between 1 and 49.
+ * To win the winning combination in the corresponding draw, 6 balls are drawn from a drum.
+ * In addition an extra ball is extracted as a complementary number, and another ball from a separate drum,
+ * between 0 and 9, which acts as a "re-entry" number.
+ * Write a program that generates 5 combinations of the primitive lottery. It has to be visualized in the way:
+ *
+ * Combination 1: 4, 2, 7,23,45,34 Complementary: 13 Reimbursement: 5
+ *
+ * Combination 2: Complementary: 12 Refund: 4
+ *
+ * Save the numbers in an ArrayList and "menéalos" them using the suffle method.
+ */
+
 package org.lasencinas;
 
 import java.util.ArrayList;
