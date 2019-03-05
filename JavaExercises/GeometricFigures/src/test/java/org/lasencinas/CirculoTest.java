28157e4d325f25b6f96bb6203049afb659f4fdca
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-05 19:12:38 +0100

    Constructor and Getters test passed

--- JavaExercises/GeometricFigures/src/test/java/org/lasencinas/CirculoTest.java
+++ JavaExercises/GeometricFigures/src/test/java/org/lasencinas/CirculoTest.java
@@ -1,20 +1,35 @@
 package org.lasencinas;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.lasencinas.circulo.Circulo;
 
-/**
- * Unit test for simple FiguraGeometrica.
- */
-public class CirculoTest
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class CirculoTest {
+
+    private Circulo circulo = null;
+    private Circulo circulo2 = null;
+    private Circulo circulo3 = null;
+
+    @Before
+    public void __init__() {
+        circulo = new Circulo();
+        circulo2 = new Circulo("Círculo", 5);
+        circulo3 = new Circulo(10);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void constructorTest() {
+        assertNotNull(circulo);
+        assertNotNull(circulo2);
+        assertNotNull(circulo3);
+    }
+
+    @Test
+    public void gettersTest() {
+        assertEquals("unknown", circulo.getNombre());
+        assertEquals("Círculo", circulo2.getNombre());
+        assertEquals(10, circulo3.getRadio(), 0.01);
     }
 }
