1b4562aef96d6cad281847a84d722e1cbe9086a4
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-05 19:35:35 +0100

    Tests passed :D

--- JavaExercises/GeometricFigures/src/test/java/org/lasencinas/CirculoTest.java
+++ JavaExercises/GeometricFigures/src/test/java/org/lasencinas/CirculoTest.java
@@ -32,4 +32,25 @@ public class CirculoTest {
         assertEquals("Círculo", circulo2.getNombre());
         assertEquals(10, circulo3.getRadio(), 0.01);
     }
+
+    @Test
+    public void settersTest() {
+        circulo.setNombre("Papagallo");
+        assertEquals("Papagallo", circulo.getNombre());
+
+        circulo.setRadio(40);
+        assertEquals(40, circulo.getRadio(), 0.01);
+
+        circulo2.setRadio(20);
+        circulo2.setNombre("Madafakerr");
+        assertEquals("Madafakerr", circulo2.getNombre());
+        assertEquals(20, circulo2.getRadio(), 0.01);
+    }
+
+    @Test
+    public void areaTest() {
+        assertEquals(0.0, circulo.area(), 0.01);
+        assertEquals(78.539, circulo2.area(), 0.01);
+        assertEquals(314.159, circulo3.area(), 0.01);
+    }
 }
