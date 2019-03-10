39ed7e084372fb19324f382c1c89c54092400238
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-10 20:24:31 +0100

    New class added

---
+++ JavaExercises/generalExercises/practiceInheritance/exercise1/src/main/java/org/lasencinas/EmpUtil.java
@@ -0,0 +1,10 @@
+package org.lasencinas;
+
+public class EmpUtil {
+    public static void printName(Employee emp) {
+        // Get the name of employee
+        String name = emp.getName();
+        // Print employee name
+        System.out.println(name);
+    }
+}
