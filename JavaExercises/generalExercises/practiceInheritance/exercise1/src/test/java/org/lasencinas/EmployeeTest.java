2ba37e1de074ef0e303635bf38b0661d9bc17753
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-10 20:41:30 +0100

    New test added to check the instanceOf operator

--- JavaExercises/generalExercises/practiceInheritance/exercise1/src/test/java/org/lasencinas/EmployeeTest.java
+++ JavaExercises/generalExercises/practiceInheritance/exercise1/src/test/java/org/lasencinas/EmployeeTest.java
@@ -14,4 +14,15 @@ public class EmployeeTest {
         assertEquals("John Jacobs", emp.getName());
         System.out.println("Employee Name: " + empName);
     }
+
+    @Test
+    public void testInstanceOf() {
+        Employee emp = new Employee();
+        emp.setName("John Jacobs");
+        Manager mgr = new Manager();
+        mgr.setName("John Jacobs");
+        System.out.println(mgr.equals(emp)); // prints true
+        System.out.println(emp.equals(mgr)); // prints true
+        System.out.println(emp.equals("John Jacobs")); // prints false
+    }
 }
