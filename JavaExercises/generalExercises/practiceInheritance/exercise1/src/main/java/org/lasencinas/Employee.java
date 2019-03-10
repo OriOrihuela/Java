b66bb00a5476460703a1663230732b112851d83f
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-10 20:42:09 +0100

    Override of equals() method with instanceOf operator

--- JavaExercises/generalExercises/practiceInheritance/exercise1/src/main/java/org/lasencinas/Employee.java
+++ JavaExercises/generalExercises/practiceInheritance/exercise1/src/main/java/org/lasencinas/Employee.java
@@ -16,4 +16,23 @@ public class Employee {
     public void setName(String name) {
         this.name = name;
     }
+
+
+    /* ---- Behaviours ---- */
+    @Override
+    public boolean equals(Object obj) {
+        boolean isEqual = false;
+        /**
+         * We compare objects of the Employee class with the objects of
+         * Employee class or its descendants
+          */
+
+        if (obj instanceof Employee) {
+            // If two have the same name, consider them equal.
+            Employee e = (Employee)obj;
+            String n = e.getName();
+            isEqual = n.equals(this.name);
+        }
+        return isEqual;
+    }
 }
