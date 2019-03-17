1f04ba0274a5ab0a136673014d229288f54a43ab
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-17 13:26:08 +0100

    Fish class tested :D

---
+++ JavaExercises/generalExercises/practiceInterfaces/exercise1/src/test/java/org/lasencinas/FishTest.java
@@ -0,0 +1,9 @@
+package org.lasencinas;
+
+public class FishTest {
+    public static void main(String[] args) {
+        Swimmable finny = new Fish("Finny");
+        finny.swim();
+        // Finny (a fish) is swimming.
+    }
+}
