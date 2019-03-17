6bbd23c0b3852e4c1e6fa50eb31f03b2ecf0182d
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-17 16:09:44 +0100

    Methods in Drawables implemented

--- JavaExercises/GeometricFigures/exercise2/src/main/java/org/lasencinas/draw/Drawables.java
+++ JavaExercises/GeometricFigures/exercise2/src/main/java/org/lasencinas/draw/Drawables.java
@@ -1,4 +1,17 @@
 package org.lasencinas.draw;
 
+import java.util.List;
+
 public interface Drawables {
+    static void dibujarDrawables(List<Drawable> list) {
+        for (Drawable drawable : list) {
+            drawable.draw();
+        }
+    }
+
+    static void applyThemeDrawables(List<Drawable> list) {
+        for (Drawable drawable : list) {
+            drawable.applyTheme();
+        }
+    }
 }
