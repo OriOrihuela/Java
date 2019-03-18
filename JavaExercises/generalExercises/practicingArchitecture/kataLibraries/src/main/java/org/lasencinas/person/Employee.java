ca190b5e4d1276c549447dd91fe08c2fbcf738f0
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-18 21:01:11 +0100

    Class done at this moment

--- JavaExercises/generalExercises/practicingArchitecture/kataLibraries/src/main/java/org/lasencinas/person/Employee.java
+++ JavaExercises/generalExercises/practicingArchitecture/kataLibraries/src/main/java/org/lasencinas/person/Employee.java
@@ -1,4 +1,38 @@
 package org.lasencinas.person;
 
-public class Employee {
+import org.lasencinas.publicEntity.Library;
+
+public class Employee extends Person {
+
+    /* ---- Properties of the class ---- */
+    private int employeeID = 0;
+    private Library idLibrary = null;
+
+
+    /* ---- Constructor ---- */
+    public Employee(String name, String surname, String dni, int employeeID, Library idLibrary) {
+        super(name, surname, dni);
+        this.employeeID = employeeID;
+        this.idLibrary = idLibrary;
+    }
+
+
+    /* ---- Getters ---- */
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public Library getIdLibrary() {
+        return idLibrary;
+    }
+
+
+    /* ---- Setters ---- */
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public void setIdLibrary(Library idLibrary) {
+        this.idLibrary = idLibrary;
+    }
 }
