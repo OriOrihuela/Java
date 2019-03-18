31b329b1f06384437a3d2324711cecc194ad4e2e
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-18 21:55:04 +0100

    Class defined

--- JavaExercises/generalExercises/practicingArchitecture/kataLibraries/src/main/java/org/lasencinas/publicEntity/PublicEntity.java
+++ JavaExercises/generalExercises/practicingArchitecture/kataLibraries/src/main/java/org/lasencinas/publicEntity/PublicEntity.java
@@ -1,4 +1,51 @@
 package org.lasencinas.publicEntity;
 
 public class PublicEntity {
+
+    /* ---- Properties of the class ---- */
+    private int entityCode = 0;
+    private String entityName = null;
+    private String address = null;
+
+
+    /* ---- Constructor ---- */
+    public PublicEntity(int entityCode, String entityName, String address) {
+        this.entityCode = entityCode;
+        this.entityName = entityName;
+        this.address = address;
+    }
+
+
+    /* ---- Getters ---- */
+    public int getEntityCode() {
+        return entityCode;
+    }
+
+    public String getEntityName() {
+        return entityName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+
+    /* ---- Setters ---- */
+    public void setEntityCode(int entityCode) {
+        this.entityCode = entityCode;
+    }
+
+    public void setEntityName(String entityName) {
+        this.entityName = entityName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    /* ---- Behaviours ---- */
+    public static void showLibrary(Library library) {
+        library.toString();
+    }
 }
