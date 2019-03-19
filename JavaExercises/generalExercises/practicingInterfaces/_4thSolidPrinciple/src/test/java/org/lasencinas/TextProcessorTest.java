bd66b3709cfcbe494e973d913abe69f9edb501af
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-19 20:09:38 +0100

    Formatted

--- JavaExercises/generalExercises/practicingInterfaces/_4thSolidPrinciple/src/test/java/org/lasencinas/TextProcessorTest.java
+++ JavaExercises/generalExercises/practicingInterfaces/_4thSolidPrinciple/src/test/java/org/lasencinas/TextProcessorTest.java
@@ -7,29 +7,29 @@ import org.junit.Test;
 
 public class TextProcessorTest {
 
-	@Test
-	public void test_simple() {
-		
-		TextProcessor procesador = new TextProcessor();
-		
-		procesador.nueva("No");
-		procesador.nueva("himporta");
-		procesador.nueva("la");
-		procesador.nueva("hortografia");
-		
-		assertEquals("No himporta la hortografia", procesador.texto());
-	}
-	
-	@Test
-	public void test_con_idioma() {
-		
-		TextProcessor procesador = new TextProcessor();
-		
-		procesador.nueva("Tengo");
-		procesador.nueva("hambre");
-		
-		assertEquals("Tengo hambre", procesador.texto());
-		
-		assertTrue(procesador.correcto(Language.ES));
-	}
+    @Test
+    public void test_simple() {
+
+        TextProcessor procesador = new TextProcessor();
+
+        procesador.nueva("No");
+        procesador.nueva("himporta");
+        procesador.nueva("la");
+        procesador.nueva("hortografia");
+
+        assertEquals("No himporta la hortografia", procesador.texto());
+    }
+
+    @Test
+    public void test_con_idioma() {
+
+        TextProcessor procesador = new TextProcessor();
+
+        procesador.nueva("Tengo");
+        procesador.nueva("hambre");
+
+        assertEquals("Tengo hambre", procesador.texto());
+
+        assertTrue(procesador.correcto(Language.ES));
+    }
 }
