24bb94dce5ac3f48bb36f01747371393ac7e630d
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-19 09:56:42 +0100

    Class done

--- JavaExercises/generalExercises/practicingArchitecture/kataLibraries/src/main/java/org/lasencinas/person/Member.java
+++ JavaExercises/generalExercises/practicingArchitecture/kataLibraries/src/main/java/org/lasencinas/person/Member.java
@@ -1,4 +1,36 @@
 package org.lasencinas.person;
 
-public class Member {
+public class Member extends Person {
+
+    /* ---- Properties of the class ---- */
+    private int memberID = 0;
+    private int idLibrary = 0;
+
+
+    /* ---- Constructor ---- */
+    public Member(String name, String surname, String dni) {
+        super(name, surname, dni);
+    }
+
+
+    /* ---- Getters ---- */
+
+    public int getMemberID() {
+        return memberID;
+    }
+
+    public int getIdLibrary() {
+        return idLibrary;
+    }
+
+
+    /* ---- Setters ---- */
+
+    public void setMemberID(int memberID) {
+        this.memberID = memberID;
+    }
+
+    public void setIdLibrary(int idLibrary) {
+        this.idLibrary = idLibrary;
+    }
 }
