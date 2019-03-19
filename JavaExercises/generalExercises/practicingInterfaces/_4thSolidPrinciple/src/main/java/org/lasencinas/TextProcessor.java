9b9c6654ebe8468473ef61f04931cfa3d95f2698
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-19 20:09:21 +0100

    Formatted

--- JavaExercises/generalExercises/practicingInterfaces/_4thSolidPrinciple/src/main/java/org/lasencinas/TextProcessor.java
+++ JavaExercises/generalExercises/practicingInterfaces/_4thSolidPrinciple/src/main/java/org/lasencinas/TextProcessor.java
@@ -7,22 +7,22 @@ import java.util.stream.Collectors;
 public class TextProcessor {
 
 
-	private List<String> texto = new ArrayList<>();
-	
-	public void nueva (String palabra) {
-		texto.add(palabra);
-	}
-	
-	public String texto () {
-		return texto.stream().collect(Collectors.joining(" "));
-	}
-	
-	public boolean correcto (Language language) {
-		for (String palabra: texto) {
-			if (! language.diccionario.contains(palabra.toLowerCase())) {
-				return false;
-			}
-		}
-		return true;
-	}
+    private List<String> texto = new ArrayList<>();
+
+    public void nueva(String palabra) {
+        texto.add(palabra);
+    }
+
+    public String texto() {
+        return texto.stream().collect(Collectors.joining(" "));
+    }
+
+    public boolean correcto(Language language) {
+        for (String palabra : texto) {
+            if (!language.diccionario.contains(palabra.toLowerCase())) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
