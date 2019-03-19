8289933ea8ef77f81bf14d80c6d55a306cd130bc
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-19 09:36:15 +0100

    Class done

--- JavaExercises/generalExercises/practicingArchitecture/kataLibraries/src/main/java/org/lasencinas/publicEntity/TownHall.java
+++ JavaExercises/generalExercises/practicingArchitecture/kataLibraries/src/main/java/org/lasencinas/publicEntity/TownHall.java
@@ -1,4 +1,53 @@
 package org.lasencinas.publicEntity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TownHall {
+
+    /* ---- Properties of the class ---- */
+    private String name = null;
+    private List<Library> libraryList = new ArrayList<>();
+
+
+    /* ---- Constructor ---- */
+    public TownHall(String name, List<Library> libraryList) {
+        this.name = name;
+        this.libraryList = libraryList;
+    }
+
+    /* ---- Getters ---- */
+    public String getName() {
+        return name;
+    }
+
+    public List<Library> getLibraryList() {
+        return libraryList;
+    }
+
+
+    /* ---- Setters ---- */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLibraryList(List<Library> libraryList) {
+        this.libraryList = libraryList;
+    }
+
+
+    /* ---- Behaviours ---- */
+    public void createLibrary() {
+        Library library = new Library();
+    }
+
+    public void addLibrary(Library library) {
+        getLibraryList().add(library);
+    }
+
+    @Override
+    public String toString() {
+        return "name: '" + getName() + '\'' +
+                ", libraryList: " + getLibraryList();
+    }
 }
