83e2bbf10066c44a374c2915fd3608a8bec8c435
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-19 20:09:09 +0100

    Formatted

--- JavaExercises/generalExercises/practicingInterfaces/_4thSolidPrinciple/src/main/java/org/lasencinas/Language.java
+++ JavaExercises/generalExercises/practicingInterfaces/_4thSolidPrinciple/src/main/java/org/lasencinas/Language.java
@@ -4,16 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum Language {
-	
-	EN ("I","am","angry"), 
-	ES ("tengo","hambre"), 
-	CA ("tenc", "fam");
-	
-	List<String> diccionario;
 
-	private Language(String ... palabras) {
-		this.diccionario = Arrays.asList(palabras);
-	}
-	
+    EN("I", "am", "hungry"),
+    ES("tengo", "hambre"),
+    CA("tenc", "fam");
 
+    List<String> diccionario;
+
+    private Language(String... palabras) {
+        this.diccionario = Arrays.asList(palabras);
+    }
 }
