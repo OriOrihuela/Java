a71837fb81e639f54a51bc15bf17056e2a4de7b2
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-20 19:23:16 +0100

    Formatted

--- JavaExercises/generalExercises/practicingInterfaces/_5thSolidPrinciple/src/main/java/org/lasencinas/bbdd/InventarioBBDD.java
+++ JavaExercises/generalExercises/practicingInterfaces/_5thSolidPrinciple/src/main/java/org/lasencinas/bbdd/InventarioBBDD.java
@@ -6,9 +6,9 @@ import org.lasencinas.negocio.Inventariable;
 public class InventarioBBDD implements Inventariable {
 
 
-	// Metodo de consulta a la base de datos
-	
-	public int numeroProductos(String tienda, String producto) {
-		return BBDD.stocs.get(tienda).get(producto);
-	}
+    // Metodo de consulta a la base de datos
+
+    public int numeroProductos(String tienda, String producto) {
+        return BBDD.stocs.get(tienda).get(producto);
+    }
 }
