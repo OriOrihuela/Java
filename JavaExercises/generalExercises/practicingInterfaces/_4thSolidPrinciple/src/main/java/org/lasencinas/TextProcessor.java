0ba33f4de7be99f697b86befb03c8a6815d3d4c8
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-20 18:32:51 +0100

    Class done

--- JavaExercises/generalExercises/practicingInterfaces/_4thSolidPrinciple/src/main/java/org/lasencinas/TextProcessor.java
+++ JavaExercises/generalExercises/practicingInterfaces/_4thSolidPrinciple/src/main/java/org/lasencinas/TextProcessor.java
@@ -1,23 +1,38 @@
 package org.lasencinas;
 
+import org.lasencinas.interfaces.Checkeable;
+import org.lasencinas.interfaces.Processable;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class TextProcessor {
+public class TextProcessor implements Checkeable, Processable {
 
-
+    /* ---- Properties of the class ---- */
     private List<String> texto = new ArrayList<>();
 
-    public void nueva(String palabra) {
-        texto.add(palabra);
+
+    /* ---- Getters ---- */
+    public List<String> getTexto() {
+        return texto;
     }
 
-    public String texto() {
+
+    /* ---- Setters ---- */
+    public void setTexto(List<String> texto) {
+        this.texto = texto;
+    }
+
+    public void addWord(String word) {
+        texto.add(word);
+    }
+
+    public String joinText() {
         return texto.stream().collect(Collectors.joining(" "));
     }
 
-    public boolean correcto(Language language) {
+    public boolean correct(Language language) {
         for (String palabra : texto) {
             if (!language.diccionario.contains(palabra.toLowerCase())) {
                 return false;
