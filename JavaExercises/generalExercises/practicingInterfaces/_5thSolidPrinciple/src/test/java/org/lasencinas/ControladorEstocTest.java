090a5884411e95c2588733a0ee8381057fc1e47b
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-20 19:23:08 +0100

    Formatted

--- JavaExercises/generalExercises/practicingInterfaces/_5thSolidPrinciple/src/test/java/org/lasencinas/ControladorEstocTest.java
+++ JavaExercises/generalExercises/practicingInterfaces/_5thSolidPrinciple/src/test/java/org/lasencinas/ControladorEstocTest.java
@@ -9,14 +9,14 @@ import org.lasencinas.negocio.Inventariable;
 
 public class ControladorEstocTest {
 
-	@Test
-	public void test_control_estoc() {
-		Inventariable inventario = new InventarioBBDD();
-		ControladorEstoc controlador = new ControladorEstoc(new InventarioBBDD());
-		
-		Assert.assertFalse(controlador.necesitaReponer("tienda norte", "mesa"));
-		Assert.assertTrue(controlador.necesitaReponer("tienda norte", "lampara"));
+    @Test
+    public void test_control_estoc() {
+        Inventariable inventario = new InventarioBBDD();
+        ControladorEstoc controlador = new ControladorEstoc(new InventarioBBDD());
 
-	}
+        Assert.assertFalse(controlador.necesitaReponer("tienda norte", "mesa"));
+        Assert.assertTrue(controlador.necesitaReponer("tienda norte", "lampara"));
+
+    }
 
 }
