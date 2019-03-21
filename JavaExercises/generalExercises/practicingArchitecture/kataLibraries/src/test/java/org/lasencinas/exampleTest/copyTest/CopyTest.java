1e55241e405531970ccc9013f1f3a721a8df76b8
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-21 22:10:02 +0100

    Test cases passed :D

--- JavaExercises/generalExercises/practicingArchitecture/kataLibraries/src/test/java/org/lasencinas/exampleTest/copyTest/CopyTest.java
+++ JavaExercises/generalExercises/practicingArchitecture/kataLibraries/src/test/java/org/lasencinas/exampleTest/copyTest/CopyTest.java
@@ -1,4 +1,47 @@
 package org.lasencinas.exampleTest.copyTest;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.lasencinas.example.Magazine;
+import org.lasencinas.example.copy.Copy;
+import org.lasencinas.publicEntity.Library;
+
+
 public class CopyTest {
+
+    private Copy copy = null;
+
+    @Before
+    public void __init__() {
+        copy = new Copy(null, null);
+    }
+
+    @Test
+    public void constructorTest() {
+        assertNotNull(copy);
+    }
+
+    @Test
+    public void gettersAndSettersTest() {
+        Magazine magazine = new Magazine();
+        Library library = new Library(998, "Biblioteca Nacional", "Avenida Papaya, 5");
+        Copy copy = new Copy(magazine, library);
+
+        assertNotNull(copy.getLibraryWhereBelongs());
+        assertNotNull(copy.getMagazine());
+
+        Magazine magazine1 = new Magazine();
+        Library library1 = new Library();
+        copy.setMagazine(magazine1);
+        copy.setLibraryWhereBelongs(library1);
+        assertEquals(magazine1, copy.getMagazine());
+        assertEquals(library1, copy.getLibraryWhereBelongs());
+    }
+
+    @Test
+    public void toStringTest() {
+        System.out.println(copy.toString());
+    }
 }
