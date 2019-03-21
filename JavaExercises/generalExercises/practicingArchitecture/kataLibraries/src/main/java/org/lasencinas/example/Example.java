7a8894735bc9691e1d28d5c033e53cc9f415ecc7
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-21 12:56:52 +0100

    Class initialized

---
+++ JavaExercises/generalExercises/practicingArchitecture/kataLibraries/src/main/java/org/lasencinas/example/Example.java
@@ -0,0 +1,56 @@
+package org.lasencinas.example;
+
+import org.lasencinas.publicEntity.TownHall;
+
+public abstract class Example {
+
+    /* ---- Properties of the class ---- */
+    private String name = null;
+    private int ISBN = 0;
+    private TownHall townHallWhereBelongs = null;
+
+
+    /* ---- Constructor ---- */
+    public Example(){}
+
+    public Example(String name, int ISBN, TownHall townHallWhereBelongs) {
+        this.name = name;
+        this.ISBN = ISBN;
+        this.townHallWhereBelongs = townHallWhereBelongs;
+    }
+
+    /* ---- Getters ---- */
+    public TownHall getTownHallWhereBelongs() {
+        return townHallWhereBelongs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+
+    /* ---- Setters ---- */
+    public void setTownHallWhereBelongs(TownHall townHallWhereBelongs) {
+        this.townHallWhereBelongs = townHallWhereBelongs;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+
+    /* ---- Behaviours ---- */
+    @Override
+    public String toString() {
+        return "townHallWhereBelongs: '" + getTownHallWhereBelongs() + "'" +
+                "\n";
+    }
+}
