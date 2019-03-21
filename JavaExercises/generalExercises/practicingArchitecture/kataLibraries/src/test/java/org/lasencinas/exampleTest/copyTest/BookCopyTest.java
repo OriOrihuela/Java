26711340366cf384552bb5b58aa070cae12a20f1
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-21 22:49:44 +0100

    Test cases passed :D

--- JavaExercises/generalExercises/practicingArchitecture/kataLibraries/src/test/java/org/lasencinas/exampleTest/copyTest/BookCopyTest.java
+++ JavaExercises/generalExercises/practicingArchitecture/kataLibraries/src/test/java/org/lasencinas/exampleTest/copyTest/BookCopyTest.java
@@ -1,4 +1,40 @@
 package org.lasencinas.exampleTest.copyTest;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.lasencinas.example.Book;
+import org.lasencinas.example.copy.BookCopy;
+import org.lasencinas.publicEntity.Library;
+
 public class BookCopyTest {
+
+    private BookCopy bookCopy = null;
+
+    @Before
+    public void __init__() {
+        Library library = new Library();
+        Book book = new Book();
+        bookCopy = new BookCopy(library, book);
+    }
+
+    @Test
+    public void constructorTest() {
+        assertNotNull(bookCopy);
+    }
+
+    @Test
+    public void gettersAndSettersTest() {
+        Book book2 = new Book();
+        Library library2 = new Library();
+        bookCopy.setBook(book2);
+        bookCopy.setLibraryWhereBelongs(library2);
+        assertEquals(book2, bookCopy.getBook());
+        assertEquals(library2, bookCopy.getLibraryWhereBelongs());
+    }
+
+    @Test
+    public void toStringTest() {
+        System.out.println(bookCopy.toString());
+    }
 }
