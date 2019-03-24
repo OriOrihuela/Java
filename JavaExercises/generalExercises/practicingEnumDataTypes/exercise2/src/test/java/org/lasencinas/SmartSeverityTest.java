1f342a8a0360c8100d4f6aad5dda0d07a8ad9029
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-24 20:48:38 +0100

    Exercise2 from book done

---
+++ JavaExercises/generalExercises/practicingEnumDataTypes/exercise2/src/test/java/org/lasencinas/SmartSeverityTest.java
@@ -0,0 +1,20 @@
+package org.lasencinas;
+
+
+public class SmartSeverityTest {
+    public static void main(String[] args) {
+        for (SmartSeverity s : SmartSeverity.values()) {
+            String name = s.name();
+            int ordinal = s.ordinal();
+            int days = s.getProjectedTurnaroundDays();
+            System.out.println("name=" + name + ", ordinal=" + ordinal + ", days=" + days);
+
+            /**
+             * name=LOW, ordinal=0, days=30
+             * name=MEDIUM, ordinal=1, days=15
+             * name=HIGH, ordinal=2, days=7
+             * name=URGENT, ordinal=3, days=1
+             */
+        }
+    }
+}
