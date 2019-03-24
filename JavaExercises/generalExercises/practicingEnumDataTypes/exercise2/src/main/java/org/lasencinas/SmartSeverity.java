a4d29bd564dbd4164c81b7a391afa937c9d840d1
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-24 20:58:59 +0100

    New comments added.

--- JavaExercises/generalExercises/practicingEnumDataTypes/exercise2/src/main/java/org/lasencinas/SmartSeverity.java
+++ JavaExercises/generalExercises/practicingEnumDataTypes/exercise2/src/main/java/org/lasencinas/SmartSeverity.java
@@ -1,15 +1,15 @@
 package org.lasencinas;
 
 public enum SmartSeverity {
-    LOW(30),
-    MEDIUM(15),
-    HIGH(7),
-    URGENT(1);
+    LOW(30),            // LOW(projectedTurnAroundDays: 30),
+    MEDIUM(15),         // MEDIUM(projectedTurnAroundDays: 15),
+    HIGH(7),            // HIGH(projectedTurnAroundDays: 7),
+    URGENT(1);          // URGENT(projectedTurnAroundDays: 1);
 
     // Declare an instance variable
     private int projectedTurnaroundDays;
 
-    // Declare a private constructor. The private modifier acces is redundant.
+    // Declare a private constructor. The private modifier access is redundant.
     private SmartSeverity(int projectedTurnaroundDays) {
         this.projectedTurnaroundDays = projectedTurnaroundDays;
     }
