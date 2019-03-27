46c2e9e00547d1588aa5d525c9f2cd4641b2e94e
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-27 20:31:01 +0100

    Enum class done.

--- JavaExercises/generalExercises/practicingRegularExpressions/RomansGoHome/src/main/java/org/lasencinas/RomanNumbers.java
+++ JavaExercises/generalExercises/practicingRegularExpressions/RomansGoHome/src/main/java/org/lasencinas/RomanNumbers.java
@@ -1,4 +1,29 @@
 package org.lasencinas;
 
 public enum RomanNumbers {
+
+    /* ---- Enum Data types ---- */
+    I(1),
+    V(5),
+    X(10),
+    L(50),
+    C(100),
+    D(500),
+    M(1000);
+
+
+    /* ---- Properties ---- */
+    private int value = 0;
+
+
+    /* ---- Constructor ---- */
+    RomanNumbers(int value) {
+        this.value = value;
+    }
+
+
+    /* ---- Getters ---- */
+    public int getValue() {
+        return value;
+    }
 }
