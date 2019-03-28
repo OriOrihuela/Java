f4e1361abda87b497d0054408a3328f930b73d7a
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-28 13:35:15 +0100

    Constructor fixed, sumRomanNumbers() test case passed.

--- JavaExercises/generalExercises/practicingRegularExpressions/RomansGoHome/src/test/java/org/lasencinas/RomanNumbersTest.java
+++ JavaExercises/generalExercises/practicingRegularExpressions/RomansGoHome/src/test/java/org/lasencinas/RomanNumbersTest.java
@@ -1,7 +1,7 @@
 package org.lasencinas;
 
-import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,7 +14,7 @@ public class RomanNumbersTest {
 
     @BeforeClass
     public static void romanNumbersSetupTest() {
-        romanNumbers = new String[7];
+        romanNumbers = new String[13];
         int romanNumbersCounter = 0;
         for (RomanNumbers number : RomanNumbers.values()) {
             romanNumbers[number.ordinal()] = number.name();
@@ -27,13 +27,13 @@ public class RomanNumbersTest {
     public void romanNumberConstructorTest() {
         RomanNumbers number = RomanNumbers.X;
         assertThat(number).isInstanceOf(RomanNumbers.class);
-        assertThat(number.ordinal()).isEqualTo(2);
+        assertThat(number.ordinal()).isEqualTo(4);
         assertThat(number.name()).isEqualToIgnoringWhitespace("X");
         assertThat(RomanNumbers.valueOf(number.name())).isEqualTo(RomanNumbers.X);
         assertThat(number.compareTo(number.X)).isEqualTo(0);
         assertThat(number.toString()).isEqualToIgnoringWhitespace("X");
         assertThat(number.equals(number.X)).isEqualTo(true);
-        assertThat(RomanNumbers.values()[2]).isEqualTo(number);
+        assertThat(RomanNumbers.values()[4]).isEqualTo(number);
     }
 
     @Test
@@ -47,5 +47,18 @@ public class RomanNumbersTest {
     public void romanNumberGetValueTest() {
         RomanNumbers number = RomanNumbers.X;
         assertThat(number.getValue()).isEqualTo(10);
+    }
+
+    @Test
+    public void sumRomanNumbersTest() {
+        String romanNumbers1 = "MMM";
+        int result = RomanNumbersUtil.sumRomanNumbers(romanNumbers1);
+        assertEquals(3000, result);
+
+        String romanNumbers2 = "MMMDCCCLXXXVIII";
+        assertEquals(3888, RomanNumbersUtil.sumRomanNumbers(romanNumbers2));
+
+        String romanNumbers3 = "MMDCCLXXVII";
+        assertEquals(2777, RomanNumbersUtil.sumRomanNumbers(romanNumbers3));
     }
 }
