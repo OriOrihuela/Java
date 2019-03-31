5d3b7cfdcd9d37d3a4872fc4911621c28e3a3f00
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-03-31 18:49:03 +0200

    subtractRomanNumbers() method implemented

--- JavaExercises/generalExercises/practicingRegularExpressions/RomansGoHome/src/main/java/org/lasencinas/RomanNumbersUtil.java
+++ JavaExercises/generalExercises/practicingRegularExpressions/RomansGoHome/src/main/java/org/lasencinas/RomanNumbersUtil.java
@@ -15,4 +15,23 @@ public class RomanNumbersUtil {
         }
         return sumCounter;
     }
+
+    public static int subtractRomanNumbers(String romanNumbers) {
+        int subtractCounter = 0;
+        for (int index = 0; index < romanNumbers.length(); index++) {
+            try {
+                Character number = romanNumbers.charAt(index);
+                Character nextNumber = romanNumbers.charAt(index + 1);
+                String romanNumber = number.toString() + nextNumber.toString();
+                for (RomanNumbers numbers : RomanNumbers.values()) {
+                    if (numbers.name().equals(romanNumber)) {
+                        subtractCounter += numbers.getValue();
+                    }
+                }
+            } catch (StringIndexOutOfBoundsException exception) {
+                //
+            }
+        }
+        return subtractCounter;
+    }
 }
